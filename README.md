0377fd7c79902c13969f6cc27b9640be5ab4f20f
HaronMan <102169938+HaronMan@users.noreply.github.com>
2025-08-25 22:06:41 +0200

    Changement nom du projet
    Sentry -> Sentinel
    
    README contient les fonctionnalités principales

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# gestionnaire-mdp
+# Sentinel
 Gestionnaire de mot de passe
 
-*Nom à trouver...*
+## Fonctionnalités
+- Stockage de mot de passe
+- Génération de mot de passe
+- Organisation de mot de passe
+- Déverouillage mot de passe maître
+- Déverouillage biométrique
+- Importation / Exportation de manière chiffré
--- sentinel/src/main/java/fr/haronman/sentinel/Main.java
+++ sentinel/src/main/java/fr/haronman/sentinel/Main.java
@@ -1,4 +1,4 @@
-package fr.haronman.sentry;
+package fr.haronman.sentinel;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -7,21 +7,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-public class Main extends Application{
-    
-    public static void main( String[] args )
-    {
+ 
+public class Main extends Application {
+    public static void main(String[] args) {
         launch(args);
     }
-
+    
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
- 
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
@@ -33,5 +31,4 @@ public class Main extends Application{
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
-    
 }
