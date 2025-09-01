5b985b980b5f804586602ea6caf187ae86ed6086
HaronMan <102169938+HaronMan@users.noreply.github.com>
2025-09-02 01:54:27 +0200

    Controller créé

---
+++ sentinel/src/main/java/fr/haronman/sentinel/controller/MainController.java
@@ -0,0 +1,22 @@
+package fr.haronman.sentinel.controller;
+
+import javafx.scene.control.Button;
+
+public class MainController {
+    
+    public Button newMDP;
+    public Button editMDP;
+    public Button deleteMDP;
+
+    public void newEntry() {
+        System.out.println("TODO Nouvelle Entree");
+    }
+
+    public void editEntry() {
+        System.out.println("TODO Editer Entree");
+    }
+
+    public void deleteEntry() {
+        System.out.println("TODO Supprimer Entree");
+    }
+}
