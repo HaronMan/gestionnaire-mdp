4ec83fd4ccdd9660c3c204d9fdeffb5654e3e163
HaronMan <102169938+HaronMan@users.noreply.github.com>
2025-09-02 01:38:04 +0200

    Fenetre sur main.fxml

--- sentinel/src/main/java/fr/haronman/sentinel/Main.java
+++ sentinel/src/main/java/fr/haronman/sentinel/Main.java
@@ -1,11 +1,10 @@
 package fr.haronman.sentinel;
 
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class Main extends Application {
@@ -14,21 +13,12 @@ public class Main extends Application {
     }
     
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/main.fxml"));
+    
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Sentinel");
+        stage.setScene(scene);
+        stage.show();
     }
 }
