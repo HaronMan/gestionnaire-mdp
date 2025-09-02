0cdb66c139f675e8dc32f0fb9bf3b97166d9d5d4
HaronMan <102169938+HaronMan@users.noreply.github.com>
2025-09-02 02:28:21 +0200

    Pop up pour ouverture de fichier
    La lecture du fichier n'est pas encore fait

---
+++ sentinel/src/main/java/fr/haronman/sentinel/FichierUtils.java
@@ -0,0 +1,17 @@
+package fr.haronman.sentinel;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import fr.haronman.sentinel.model.MotDePasse;
+
+public class FichierUtils {
+
+    public static ArrayList<MotDePasse> getMdpsFromFichier(File fichier) {
+        ArrayList<MotDePasse> mdps = new ArrayList<MotDePasse>();
+        System.out.println("TODO Traitement fichier");
+        
+        return mdps;
+    }
+    
+}
--- sentinel/src/main/java/fr/haronman/sentinel/controller/MainController.java
+++ sentinel/src/main/java/fr/haronman/sentinel/controller/MainController.java
@@ -1,12 +1,33 @@
 package fr.haronman.sentinel.controller;
 
+import java.io.File;
+
+import fr.haronman.sentinel.FichierUtils;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 
 public class MainController {
     
+    public Button openMDP;
     public Button newMDP;
     public Button editMDP;
     public Button deleteMDP;
+
+    public void openFile() {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Ouvrir un fichier");
+
+        // Ouverture sur Documents par défaut
+        fc.setInitialDirectory(new File(System.getProperty("user.home"), "Documents"));
+
+        FileChooser.ExtensionFilter ext = new FileChooser.ExtensionFilter("JSON (*.json)", "*.json");
+        fc.getExtensionFilters().add(ext);
+
+        File fichier = fc.showOpenDialog(openMDP.getScene().getWindow());
+        if (fichier != null) {
+            FichierUtils.getMdpsFromFichier(fichier);
+        }
+    }
 
     public void newEntry() {
         System.out.println("TODO Nouvelle Entree");
