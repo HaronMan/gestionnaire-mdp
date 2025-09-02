6fd3241f044eadfed88916564b5f9d4ac26b47cc
HaronMan <102169938+HaronMan@users.noreply.github.com>
2025-09-02 22:35:43 +0200

    Importation JSON et affichage tableau

--- sentinel/src/main/java/fr/haronman/sentinel/FichierUtils.java
+++ sentinel/src/main/java/fr/haronman/sentinel/FichierUtils.java
@@ -1,17 +1,45 @@
 package fr.haronman.sentinel;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import fr.haronman.sentinel.model.MotDePasse;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class FichierUtils {
 
-    public static ArrayList<MotDePasse> getMdpsFromFichier(File fichier) {
+    public static ObservableList<MotDePasse> getMdpsFromFichier(File fichier) throws IOException {
         ArrayList<MotDePasse> mdps = new ArrayList<MotDePasse>();
-        System.out.println("TODO Traitement fichier");
+
+        // Récupération en texte du contenu du fichier puis conversion en liste
+        String content = Files.readString(fichier.toPath(), StandardCharsets.UTF_8);
+        JSONArray json = new JSONArray(content);
         
-        return mdps;
+        // Ajout des elements dans la liste
+        if (json.length() > 0) {
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject item = json.getJSONObject(i);
+                mdps.add(
+                    new MotDePasse(
+                        item.getString("titre"),
+                        item.getString("login"),
+                        item.getString("mdp"),
+                        item.getString("url"),
+                        item.getString("note")
+                    )
+                );
+                System.out.println(item);
+            }
+        }
+
+        return FXCollections.observableArrayList(mdps);
     }
     
 }
--- sentinel/src/main/java/fr/haronman/sentinel/controller/MainController.java
+++ sentinel/src/main/java/fr/haronman/sentinel/controller/MainController.java
@@ -1,9 +1,16 @@
 package fr.haronman.sentinel.controller;
 
 import java.io.File;
+import java.io.IOException;
 
 import fr.haronman.sentinel.FichierUtils;
+import fr.haronman.sentinel.model.MotDePasse;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
 
 public class MainController {
@@ -13,19 +20,36 @@ public class MainController {
     public Button editMDP;
     public Button deleteMDP;
 
-    public void openFile() {
+    @FXML
+    public TableView table;
+    @FXML
+    private TableColumn<MotDePasse, String> titre;
+    @FXML
+    private TableColumn<MotDePasse, String> login;
+    @FXML
+    private TableColumn<MotDePasse, String> mdp;
+    @FXML
+    private TableColumn<MotDePasse, String> url;
+    @FXML
+    private TableColumn<MotDePasse, String> note;
+
+    public void openFile() throws IOException {
         FileChooser fc = new FileChooser();
         fc.setTitle("Ouvrir un fichier");
 
         // Ouverture sur Documents par défaut
         fc.setInitialDirectory(new File(System.getProperty("user.home"), "Documents"));
 
+        // Uniquement les JSONs (A MODIFIER PLUS TARD)
         FileChooser.ExtensionFilter ext = new FileChooser.ExtensionFilter("JSON (*.json)", "*.json");
         fc.getExtensionFilters().add(ext);
 
         File fichier = fc.showOpenDialog(openMDP.getScene().getWindow());
         if (fichier != null) {
-            FichierUtils.getMdpsFromFichier(fichier);
+
+            // Lecture fichier
+            ObservableList<MotDePasse> mdps = FichierUtils.getMdpsFromFichier(fichier);
+            table.setItems(mdps);
         }
     }
 
@@ -39,5 +63,14 @@ public class MainController {
 
     public void deleteEntry() {
         System.out.println("TODO Supprimer Entree");
+    }
+
+    @FXML
+    private void initialize() {
+        titre.setCellValueFactory(new PropertyValueFactory<>("titre"));
+        login.setCellValueFactory(new PropertyValueFactory<>("login"));
+        mdp.setCellValueFactory(new PropertyValueFactory<>("mdp"));
+        url.setCellValueFactory(new PropertyValueFactory<>("url"));
+        note.setCellValueFactory(new PropertyValueFactory<>("note"));
     }
 }
