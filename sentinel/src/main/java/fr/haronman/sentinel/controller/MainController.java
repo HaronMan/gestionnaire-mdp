c2ecd97cb8c6fba193e8bab367a02aeb153491a4
HaronMan <102169938+HaronMan@users.noreply.github.com>
2025-09-03 00:07:10 +0200

    Ouverture du formulaire d'une nouvelle entrée possible
    - Bouton Annuler fait

--- sentinel/src/main/java/fr/haronman/sentinel/controller/MainController.java
+++ sentinel/src/main/java/fr/haronman/sentinel/controller/MainController.java
@@ -7,11 +7,16 @@ import fr.haronman.sentinel.FichierUtils;
 import fr.haronman.sentinel.model.MotDePasse;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MainController {
     @FXML
@@ -61,8 +66,16 @@ public class MainController {
         }
     }
 
-    public void newEntry() {
-        System.out.println("TODO Nouvelle Entree");
+    public void newEntry() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/new_entry.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = new Stage();
+        stage.setTitle("Nouvelle Entrée");
+        stage.setScene(new Scene(root));
+
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
     }
 
     public void editEntry() {
--- sentinel/src/main/java/fr/haronman/sentinel/controller/NewEntryController.java
+++ sentinel/src/main/java/fr/haronman/sentinel/controller/NewEntryController.java
@@ -1,5 +1,26 @@
 package fr.haronman.sentinel.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class NewEntryController {
+
+    public Button cancel;
     
+    @FXML
+    public void handleMDP() {
+
+    }
+
+    @FXML
+    public void handleConfirmMDP() {
+        
+    }
+
+    @FXML
+    public void closeForm() {
+        Stage stage = (Stage) cancel.getScene().getWindow();
+        stage.close();
+    }
 }
